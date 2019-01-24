b0df4f7110b83d1f147aca1ddc43b56e9409d1e8
harika91 <harika91@users.noreply.github.com>
2019-01-24 08:43:44 -0600

    Update GeneDAO.java

--- src/edu/mcw/rgd/dao/impl/GeneDAO.java
+++ src/edu/mcw/rgd/dao/impl/GeneDAO.java
@@ -602,7 +602,7 @@ public class GeneDAO extends AbstractDAO {
         String query = "SELECT g.rgd_id FROM genes g, rgd_ids r "+
                 "WHERE g.gene_symbol_lc=IN ("+
                 Utils.concatenate(",", geneSymbols, "toLowerCase", "'")+
-                "AND g.rgd_id=r.rgd_id AND r.species_type_key=? and r.object_status='ACTIVE'";
+                ") AND g.rgd_id=r.rgd_id AND r.species_type_key=? and r.object_status='ACTIVE'";
 
         return IntListQuery.execute(this, query, speciesKey);
     }
