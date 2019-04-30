35ecc020d9a8a6f015b4b4dd0f3f7e26a93a223c
Marek Tutaj <mtutaj@mcw.edu>
2019-04-30 15:25:48 -0500

    added API for OMIM table

---
+++ src/edu/mcw/rgd/dao/spring/OmimQuery.java
@@ -0,0 +1,38 @@
+package edu.mcw.rgd.dao.spring;
+
+import edu.mcw.rgd.dao.AbstractDAO;
+import edu.mcw.rgd.datamodel.Omim;
+import org.springframework.jdbc.object.MappingSqlQuery;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author mtutaj
+ * @since Apr 30, 2019
+ */
+public class OmimQuery extends MappingSqlQuery  {
+
+    public OmimQuery(DataSource ds, String query) {
+        super(ds, query);
+    }
+
+    protected Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Omim o = new Omim();
+
+        o.setMimNumber(rs.getString("mim_number"));
+        o.setStatus(rs.getString("status"));
+        o.setPhenotype(rs.getString("phenotype"));
+        o.setCreatedDate(rs.getTimestamp("created_date"));
+        o.setLastModifiedDate(rs.getTimestamp("last_modified_date"));
+
+        return o;
+    }
+
+    public static List<Omim> execute(AbstractDAO dao, String sql, Object... params) throws Exception {
+        OmimQuery q = new OmimQuery(dao.getDataSource(), sql);
+        return dao.execute(q, params);
+    }
+}
---
+++ src/edu/mcw/rgd/datamodel/Omim.java
@@ -0,0 +1,77 @@
+package edu.mcw.rgd.datamodel;
+
+import java.util.Date;
+
+/**
+ * @author mtutaj
+ * @since Apr 30, 2019
+ * Represents a row in OMIM table
+ */
+public class Omim {
+    private String mimNumber;
+    private String status; // 'live', etc
+    private String phenotype; // if available
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Omim omim = (Omim) o;
+
+        if (!mimNumber.equals(omim.mimNumber)) return false;
+        if (!status.equals(omim.status)) return false;
+        return phenotype.equals(omim.phenotype);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mimNumber.hashCode();
+        result = 31 * result + status.hashCode();
+        result = 31 * result + phenotype.hashCode();
+        return result;
+    }
+
+    public String getMimNumber() {
+        return mimNumber;
+    }
+
+    public void setMimNumber(String mimNumber) {
+        this.mimNumber = mimNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPhenotype() {
+        return phenotype;
+    }
+
+    public void setPhenotype(String phenotype) {
+        this.phenotype = phenotype;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
