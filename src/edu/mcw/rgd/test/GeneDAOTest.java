989fe4f8a982b16e8aaab1e7c8065787612e1486
Marek Tutaj <mtutaj@mcw.edu>
2019-07-26 14:04:42 -0500

    added GeneDAO.getGenesForProteinDomain()

---
+++ src/edu/mcw/rgd/test/GeneDAOTest.java
@@ -0,0 +1,42 @@
+package edu.mcw.rgd.test;
+
+import edu.mcw.rgd.dao.impl.GeneDAO;
+import edu.mcw.rgd.dao.impl.MapDAO;
+import edu.mcw.rgd.datamodel.Gene;
+import edu.mcw.rgd.datamodel.Map;
+import edu.mcw.rgd.datamodel.MapData;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mtutaj
+ * @since Sep 13, 2010
+ */
+public class GeneDAOTest extends TestCase {
+
+    GeneDAO dao = new GeneDAO();
+
+    public GeneDAOTest(String testName) {
+        super(testName);
+    }
+
+    public void testAll() throws Exception {
+
+        int rgdId = 13838876;
+        List<Gene> genes = dao.getGenesForProteinDomain(rgdId);
+        assert genes.size() > 0;
+
+        List<Integer> speciesTypeKeys = new ArrayList<>();
+        speciesTypeKeys.add(1);
+        speciesTypeKeys.add(2);
+        speciesTypeKeys.add(3);
+        List<String> evidenceCodes = new ArrayList<>();
+        evidenceCodes.add("IAGP");
+        evidenceCodes.add("IDA");
+        genes = dao.getAnnotatedGenesAndOrthologs("DOID:14221", speciesTypeKeys, evidenceCodes);
+        assert genes.size() > 0;
+    }
+
+ }
