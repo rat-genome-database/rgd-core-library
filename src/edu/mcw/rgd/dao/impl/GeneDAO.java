4ce6b5e53ca7efddb056e6d9fb11c6e67eccace4
jdepons <jdepons@mcw.edu>
2019-11-19 12:50:51 -0600

    readded MappedGene call

--- src/edu/mcw/rgd/dao/impl/GeneDAO.java
+++ src/edu/mcw/rgd/dao/impl/GeneDAO.java
@@ -304,7 +304,7 @@ public class GeneDAO extends AbstractDAO {
         return GeneQuery.execute(this, query, chr, stopPos, startPos, mapKey);
     }
 
-    public List<MappedGenePosition> getActiveMappedGenes(String chr, long startPos, long stopPos, int mapKey) throws Exception {
+    public List<MappedGenePosition> getActiveMappedGenePositions(String chr, long startPos, long stopPos, int mapKey) throws Exception {
         String query = "SELECT g.rgd_id as rgd_id, g.gene_symbol as symbol, r.species_type_key, md.* \n" +
                 "FROM genes g, rgd_ids r, maps_data md \n" +
                 "WHERE r.object_status='ACTIVE' AND r.rgd_id=g.rgd_id AND md.rgd_id=g.rgd_id \n"+
@@ -314,6 +314,15 @@ public class GeneDAO extends AbstractDAO {
         return MappedGenePositionQuery.run(this, query, chr, stopPos, startPos, mapKey);
     }
 
+    public List<MappedGene> getActiveMappedGene(String chr, long startPos, long stopPos, int mapKey) throws Exception {
+        String query = "SELECT g.rgd_id as rgd_id, g.gene_symbol as symbol, r.species_type_key, md.* \n" +
+                "FROM genes g, rgd_ids r, maps_data md \n" +
+                "WHERE r.object_status='ACTIVE' AND r.rgd_id=g.rgd_id AND md.rgd_id=g.rgd_id \n"+
+                " AND md.chromosome=? AND md.start_pos<=? AND md.stop_pos>=? AND md.map_key=? \n"+
+                "ORDER BY md.start_pos";
+
+        return MappedGeneQuery.run(this, query, chr, stopPos, startPos, mapKey);
+    }
 
     public List<MappedGene> getActiveMappedGenes(int mapKey, List<String> geneSymbols) throws Exception {
 
