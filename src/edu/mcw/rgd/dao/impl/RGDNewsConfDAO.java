646eb996426566178b5fba9e55b61eca131ea27e
Lamers <llamers@mcw.edu>
2020-12-09 09:54:47 -0600

    created Query, DAO, and Datamodel for Table RGD_NEWS_CONFERENCES

---
+++ src/edu/mcw/rgd/dao/impl/RGDNewsConfDAO.java
@@ -0,0 +1,43 @@
+package edu.mcw.rgd.dao.impl;
+
+import edu.mcw.rgd.dao.AbstractDAO;
+import edu.mcw.rgd.dao.spring.RGDNewsConfQuery;
+import edu.mcw.rgd.datamodel.RGDNewsConf;
+
+import java.util.List;
+
+
+public class RGDNewsConfDAO extends AbstractDAO {
+    public RGDNewsConfDAO() {}
+
+    public List<RGDNewsConf> getAllNews() throws Exception {
+        String query = "select * from RGD_NEWS_CONFERENCES where CONTENT_TYPE='NEWS'";
+        return RGDNewsConfQuery.execute(this,query);
+    }
+
+    public List<RGDNewsConf> getAllConferences() throws Exception {
+        String query = "select * from RGD_NEWS_CONFERENCES where CONTENT_TYPE='CONFERENCES'";
+        return RGDNewsConfQuery.execute(this,query);
+    }
+
+    public List<RGDNewsConf> getAllVideos() throws Exception {
+        String query = "select * from RGD_NEWS_CONFERENCES where CONTENT_TYPE='VIDEO'";
+        return RGDNewsConfQuery.execute(this,query);
+    }
+
+    public int deleteRGDNewsConf(int newsId) throws Exception{
+        String sql = "DELETE FROM RGD_NEWS_CONFERENCES WHERE NEWS_ID=?";
+        return update(sql, newsId);
+    }
+
+    public void insertIntoRGDNewsConf(RGDNewsConf newsConf) throws Exception {
+        int key = this.getNextKeyFromSequence("RGD_NEWS_CONFERENCES_SEQ");
+        newsConf.setNewsId(key);
+        String qeury = "insert into RGD_NEWS_CONFERENCES (news_id, display_text, redirect_link, CONTENT_TYPE) " +
+                "values (?, ?, ?, upper(?))";
+        update(qeury, newsConf.getNewsId(), newsConf.getDisplayText(), newsConf.getRedirectLink(), newsConf.getContentType());
+
+        return;
+    }
+
+}
---
+++ src/edu/mcw/rgd/datamodel/RGDNewsConf.java
@@ -0,0 +1,44 @@
+package edu.mcw.rgd.datamodel;
+
+public class RGDNewsConf {
+    private int newsId;
+    private String displayText;
+    private String redirectLink;
+    private String contentType;
+
+    public RGDNewsConf() {}
+
+    public int getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    public void setDisplayText(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getRedirectLink() {
+        return redirectLink;
+    }
+
+    public void setRedirectLink(String redirectLink) {
+        this.redirectLink = redirectLink;
+    }
+
+
+}
