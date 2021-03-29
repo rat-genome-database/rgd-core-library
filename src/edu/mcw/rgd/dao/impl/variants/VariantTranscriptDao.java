4b81f9b40f116a3bf82643222a888b910e4b4215
Thota <jthota@mcw.edu>
2021-03-29 16:54:15 -0500

    updated vv core

---
+++ src/edu/mcw/rgd/dao/impl/variants/VariantTranscriptDao.java
@@ -0,0 +1,24 @@
+package edu.mcw.rgd.dao.impl.variants;
+
+import edu.mcw.rgd.dao.AbstractDAO;
+import edu.mcw.rgd.dao.DataSourceFactory;
+import edu.mcw.rgd.dao.spring.variants.VariantTranscriptQuery;
+import edu.mcw.rgd.datamodel.variants.VariantTranscript;
+import org.springframework.jdbc.core.SqlParameter;
+
+import java.sql.Types;
+import java.util.List;
+
+public class VariantTranscriptDao extends AbstractDAO {
+    public List<edu.mcw.rgd.datamodel.variants.VariantTranscript> getVariantTranscripts(long rgdId, int mapKey) throws Exception {
+        String sql=" select t.*, p.prediction from variant_transcript t left outer join " +
+                "                polyphen p on (t.variant_rgd_id=p.variant_rgd_id and t.transcript_rgd_id=p.transcript_rgd_id)\n" +
+                "                where t.variant_rgd_id=? " +
+                "                and t.map_key=?";
+        VariantTranscriptQuery q=new VariantTranscriptQuery(DataSourceFactory.getInstance().getCarpeNovoDataSource(), sql);
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+
+        return q.execute(rgdId, mapKey);
+    }
+}
