6d8ed24defa4dcabe95895845b402e222fcfe783
Lamers <llamers@mcw.edu>
2021-04-12 13:04:19 -0500

    fixing a bug with webfeedback

--- src/edu/mcw/rgd/dao/impl/WebFeedbackDAO.java
+++ src/edu/mcw/rgd/dao/impl/WebFeedbackDAO.java
@@ -1,16 +1,21 @@
 package edu.mcw.rgd.dao.impl;
 
 import edu.mcw.rgd.dao.AbstractDAO;
+import edu.mcw.rgd.dao.DataSourceFactory;
 
+import javax.sql.DataSource;
 import java.sql.Date;
 
 public class WebFeedbackDAO extends AbstractDAO {
     public WebFeedbackDAO() {}
+    public DataSource getDataSource() throws Exception{
+        return DataSourceFactory.getInstance().getRgdFbDataSource();
+    }
     public void insertLike(String page) throws Exception {
         java.util.Date today = new java.util.Date();
         Date date = new Date(today.getTime());
 
-        String sql = "insert into website_feedback (thumbs_up, date_liked, web_page) values (1, to_date(?, 'yyyy-MM-dd'), ?)";
+        String sql = "insert into WEBSITE_FEEDBACK (THUMBS_UP, DATE_LIKED, WEB_PAGE) values (1, to_date(?, 'yyyy-MM-dd'), ?)";
         String curDate = date.toString();
 
         update(sql,curDate, page);
@@ -21,7 +26,7 @@ public class WebFeedbackDAO extends AbstractDAO {
         java.util.Date today = new java.util.Date();
         Date date = new Date(today.getTime());
 
-        String sql = "insert into website_feedback (thumbs_down, date_liked, web_page) values (1, to_date(?, 'yyyy-MM-dd'), ?)";
+        String sql = "insert into WEBSITE_FEEDBACK (THUMBS_DOWN, DATE_LIKED, WEB_PAGE) values (1, to_date(?, 'yyyy-MM-dd'), ?)";
         String curDate = date.toString();
 
         update(sql,curDate, page);
