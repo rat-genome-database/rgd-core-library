9f83e2a2e06584b07a155da88b9fec2f0accda1a
Thota <jthota@mcw.edu>
2021-09-27 14:38:19 -0500

    updated sampleDAO, added getSampleByStrainIdsNmapkey

--- src/edu/mcw/rgd/dao/impl/SampleDAO.java
+++ src/edu/mcw/rgd/dao/impl/SampleDAO.java
@@ -129,6 +129,16 @@ public class SampleDAO extends JdbcBaseDAO {
         List<Sample> samples = q.execute(strainRgdId, patientId);
        return samples.isEmpty() ? null : samples.get(0);
     }
+    public Sample getSampleByStrainRgdIdNMapKey(int strainRgdId, int mapKey) {
+        String sql = "SELECT * FROM sample WHERE strain_rgd_id=? and patient_id in (" +
+                "select patient_id from patient where mapKey=?)";
+        SampleQuery q = new SampleQuery(this.getDataSource(), sql);
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.compile();
+        List<Sample> samples = q.execute(strainRgdId, mapKey);
+        return samples.isEmpty() ? null : samples.get(0);
+    }
 
     public List<Sample> getSamplesByStrainRgdId(int strainRgdId) {
         String sql = "SELECT * FROM sample WHERE strain_rgd_id=?";
