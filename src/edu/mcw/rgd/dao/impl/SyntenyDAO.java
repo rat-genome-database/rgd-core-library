9de95e96dea61860613bc75926883e10babd4a46
tutajm <mtutaj@mcw.edu>
2021-11-11 12:19:14 -0600

    synteny: changed sorting

--- src/edu/mcw/rgd/dao/impl/SyntenyDAO.java
+++ src/edu/mcw/rgd/dao/impl/SyntenyDAO.java
@@ -3,6 +3,7 @@ package edu.mcw.rgd.dao.impl;
 import edu.mcw.rgd.dao.AbstractDAO;
 import edu.mcw.rgd.dao.spring.SyntenyQuery;
 import edu.mcw.rgd.datamodel.SyntenicRegion;
+import edu.mcw.rgd.process.mapping.MapManager;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class SyntenyDAO extends AbstractDAO {
     public List<SyntenicRegion> getBlocks(int backboneMapKey, String chromosome, int start, int stop, int mapKey) throws Exception {
         String query = "SELECT * FROM synteny_ucsc WHERE map_key1=? AND map_key2=? " +
                 " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? " +
-                "ORDER BY chromosome2, start_pos2";
+                "ORDER BY chromosome1, start_pos1";
         SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
         return execute(q, backboneMapKey, mapKey, stop, start, chromosome);
     }
@@ -23,8 +24,10 @@ public class SyntenyDAO extends AbstractDAO {
     public List<SyntenicRegion> getBlocks(int backboneMapKey, String chromosome, int start, int stop, int mapKey, int chainLevel) throws Exception {
         String query = "SELECT * FROM synteny_ucsc WHERE map_key1=? AND map_key2=? " +
                 " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level=?" +
-                "ORDER BY chromosome2, start_pos2";
+                "ORDER BY chromosome1, start_pos1";
         SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
         return execute(q, backboneMapKey, mapKey, stop, start, chromosome, chainLevel);
     }
+
+
 }
