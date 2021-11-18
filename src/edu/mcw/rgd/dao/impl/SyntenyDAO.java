4f71037bc966ae6da9530a869b4e102f06d06be3
tutajm <mtutaj@mcw.edu>
2021-11-18 17:29:50 -0600

    SyntenyDAO: added getSizedGaps()

--- src/edu/mcw/rgd/dao/impl/SyntenyDAO.java
+++ src/edu/mcw/rgd/dao/impl/SyntenyDAO.java
@@ -73,7 +73,7 @@ public class SyntenyDAO extends AbstractDAO {
 
     public List<SyntenicRegion> getGaps(int backboneMapKey, String chromosome, int start, int stop, int mapKey, int chainLevel) throws Exception {
         String query = "SELECT * FROM synteny_ucsc_gaps WHERE map_key1=? AND map_key2=? " +
-                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level=?" +
+                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level=? " +
                 "ORDER BY chromosome1, start_pos1";
         SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
         return execute(q, backboneMapKey, mapKey, stop, start, chromosome, chainLevel);
@@ -81,9 +81,34 @@ public class SyntenyDAO extends AbstractDAO {
 
     public List<SyntenicRegion> getGaps(int backboneMapKey, String chromosome, int start, int stop, int mapKey, int minChainLevel, int maxChainLevel) throws Exception {
         String query = "SELECT * FROM synteny_ucsc_gaps WHERE map_key1=? AND map_key2=? " +
-                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level BETWEEN ? and ?" +
+                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level BETWEEN ? and ? " +
                 "ORDER BY chromosome1, start_pos1";
         SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
         return execute(q, backboneMapKey, mapKey, stop, start, chromosome, minChainLevel, maxChainLevel);
+    }
+
+
+    public List<SyntenicRegion> getSizedGaps(int backboneMapKey, String chromosome, int start, int stop, int backboneGapMinSize, int mapKey) throws Exception {
+        String query = "SELECT * FROM synteny_ucsc_gaps WHERE map_key1=? AND map_key2=? " +
+                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND pos_len1>? " +
+                "ORDER BY chromosome1, start_pos1";
+        SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
+        return execute(q, backboneMapKey, mapKey, stop, start, chromosome, backboneGapMinSize);
+    }
+
+    public List<SyntenicRegion> getSizedGaps(int backboneMapKey, String chromosome, int start, int stop, int backboneGapMinSize, int mapKey, int chainLevel) throws Exception {
+        String query = "SELECT * FROM synteny_ucsc_gaps WHERE map_key1=? AND map_key2=? " +
+                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level=? AND pos_len1>? " +
+                "ORDER BY chromosome1, start_pos1";
+        SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
+        return execute(q, backboneMapKey, mapKey, stop, start, chromosome, chainLevel, backboneGapMinSize);
+    }
+
+    public List<SyntenicRegion> getSizedGaps(int backboneMapKey, String chromosome, int start, int stop, int backboneGapMinSize, int mapKey, int minChainLevel, int maxChainLevel) throws Exception {
+        String query = "SELECT * FROM synteny_ucsc_gaps WHERE map_key1=? AND map_key2=? " +
+                " AND start_pos1<? AND stop_pos1 > ? AND chromosome1=? AND chain_level BETWEEN ? and ? AND pos_len1>? " +
+                "ORDER BY chromosome1, start_pos1";
+        SyntenyQuery q = new SyntenyQuery(this.getDataSource(), query);
+        return execute(q, backboneMapKey, mapKey, stop, start, chromosome, minChainLevel, maxChainLevel, backboneGapMinSize);
     }
 }
