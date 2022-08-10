35c67d93548d083724c1953edc6f4775638781e1
llamersmcw <llamers@mcw.edu>
2022-08-10 11:11:09 -0500

    get variants if exon or intron

--- src/edu/mcw/rgd/dao/impl/variants/VariantDAO.java
+++ src/edu/mcw/rgd/dao/impl/variants/VariantDAO.java
@@ -181,6 +181,25 @@ public class VariantDAO extends AbstractDAO {
         return q.execute(mapKey,chrom,start,stop, offset);
     }
 
+    public List<VariantMapData> getVariantsWithTranscriptLocationName(int mapKey, String chrom, int start, int stop, String locName, int offset) throws Exception{
+        String loc = "";
+        if (locName.equals("Exon"))
+            loc = "EXON";
+        else
+            loc="INTRON";
+        String sql = "select * from variant v, variant_map_data vm where v.rgd_id=vm.rgd_id and and vm.map_key=? v.rgd_id in (\n" +
+                "select distinct variant_rgd_id as rgd_id from variant_transcript where location_name like '%"+loc+"%' and variant_rgd_id in " +
+                "(select * from variant v, variant_map_data vm where v.rgd_id=vm.rgd_id  and vm.chromosome=? and vm.start_pos between ? and ?) ) " +
+                "offset ? rows fetch next 1000 rows only";
+        VariantMapQuery q= new VariantMapQuery(DataSourceFactory.getInstance().getCarpeNovoDataSource(),sql);
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.VARCHAR));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        q.declareParameter(new SqlParameter(Types.INTEGER));
+        return q.execute(mapKey,chrom,start,stop, offset);
+    }
+
     public Integer getVariantsCountWithGeneLocation(int mapKey, String chrom, int start, int stop) throws Exception{
         String sql = "select count(*) as CNT from variant v, variant_map_data vm where v.rgd_id=vm.rgd_id and vm.map_key=? and vm.chromosome=? and vm.start_pos between ? and ?";
         Connection con = DataSourceFactory.getInstance().getCarpeNovoDataSource().getConnection();
