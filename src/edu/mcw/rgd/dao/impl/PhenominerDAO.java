09558aca082b6e22d071c971fc51b62ecc6421d3
Thota <jthota@mcw.edu>
2022-09-16 16:10:04 -0500

    added unit conversion factor method

--- src/edu/mcw/rgd/dao/impl/PhenominerDAO.java
+++ src/edu/mcw/rgd/dao/impl/PhenominerDAO.java
@@ -2774,4 +2774,10 @@ public class PhenominerDAO extends AbstractDAO {
         }
         return recList;
     }
+    public List<PhenominerUnitTable> getConversionFactorToStandardUnits(int recordId) throws Exception {
+        String sql="select * from phenominer_term_unit_scales tus where unit_from in (\n" +
+                "select measurement_units from experiment_record er where experiment_record_id=?)";
+        PhenominerUnitTablesQuery query=new PhenominerUnitTablesQuery(this.getDataSource(), sql);
+        return execute(query, recordId);
+    }
 }
