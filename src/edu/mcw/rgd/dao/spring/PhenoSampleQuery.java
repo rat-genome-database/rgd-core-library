31f866200fe4a77517de2762e799de0ca93cad35
llamersmcw <llamers@mcw.edu>
2022-10-18 12:12:58 -0500

    Update PhenoSampleQuery.java

--- src/edu/mcw/rgd/dao/spring/PhenoSampleQuery.java
+++ src/edu/mcw/rgd/dao/spring/PhenoSampleQuery.java
@@ -32,7 +32,7 @@ public class PhenoSampleQuery extends MappingSqlQuery {
         s.setCellLineId(rs.getString("cell_line_id"));
         s.setGeoSampleAcc(rs.getString("geo_sample_acc"));
         s.setBioSampleId(rs.getString("biosample_id"));
-        s.setDevelopmentalStage(rs.getString("life_stage"));
+        s.setLifeStage(rs.getString("life_stage"));
         s.setLastModifiedBy(rs.getString("last_modified_by"));
         s.setCreatedBy(rs.getString("created_by"));
         return s;
