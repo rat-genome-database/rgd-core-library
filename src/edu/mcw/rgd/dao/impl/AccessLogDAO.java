98298a1d445a70b9cda19c556f9b5e3e12c0d890
jdepons <jdepons@mcw.edu>
2023-03-22 10:51:06 -0500

    udpated

--- src/edu/mcw/rgd/dao/impl/AccessLogDAO.java
+++ src/edu/mcw/rgd/dao/impl/AccessLogDAO.java
@@ -6,6 +6,8 @@ public class AccessLogDAO extends AbstractDAO {
 
     public int log(String type, String description) throws Exception {
         String sql = "insert into ACCESS_LOG (log_type,log_date,description) values (?,?,?)";
-        return update(type, new java.sql.Date(new java.util.Date().getTime()), description);
+        java.util.Date today = new java.sql.Date(new java.util.Date().getTime());
+
+        return update(sql,type, today, description);
     }
 }
