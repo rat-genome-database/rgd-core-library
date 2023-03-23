dddd8ebb6f0a5385447fe53e99149d71180ff695
jdepons <jdepons@mcw.edu>
2023-03-23 13:07:44 -0500

    added logging object

---
+++ src/edu/mcw/rgd/dao/impl/AccessLogDAO.java
@@ -0,0 +1,13 @@
+package edu.mcw.rgd.dao.impl;
+import edu.mcw.rgd.dao.AbstractDAO;
+import java.util.Date;
+
+public class AccessLogDAO extends AbstractDAO {
+
+    public int log(String type, String description) throws Exception {
+        String sql = "insert into ACCESS_LOG (log_type,log_date,description) values (?,?,?)";
+        java.util.Date today = new java.sql.Date(new java.util.Date().getTime());
+
+        return updateFb(sql,type, today, description);
+    }
+}
