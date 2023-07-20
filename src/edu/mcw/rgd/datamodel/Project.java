1037377c81a15abfe674e585a9b6369b4e599dfb
Kundurthi <akundurthi@mcw.edu>
2023-07-20 11:54:19 -0500

    Adding the Project data model

---
+++ src/edu/mcw/rgd/datamodel/Project.java
@@ -0,0 +1,34 @@
+package edu.mcw.rgd.datamodel;
+
+public class Project {
+
+    private int rgdid;
+    private String desc;
+    private String name;
+
+    public int getRgdid() {
+        return rgdid;
+    }
+
+    public void setRgdid(int rgdid) {
+        this.rgdid = rgdid;
+    }
+
+    public String getName() {
+        return nam.je;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+}
