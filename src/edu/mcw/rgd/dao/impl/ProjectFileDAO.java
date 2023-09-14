edc338c9d97b7cc6300ecfef1bdcffe92fac7016
Akhilanand K <akundurthi@mcw.edu>
2023-09-14 16:34:07 -0500

    modified ProjectFileDAO with insert, update and delete methods

--- src/edu/mcw/rgd/dao/impl/ProjectFileDAO.java
+++ src/edu/mcw/rgd/dao/impl/ProjectFileDAO.java
@@ -19,5 +19,20 @@ public class ProjectFileDAO extends AbstractDAO{
         String query="select * from project_files where rgd_id=?";
         return ProjectFileQuery.execute(this,query,projectID);
     }
+    public int insert(ProjectFile projectFile) throws Exception {
+        String query = "INSERT INTO PROJECT_FILES (FILE_KEY, RGD_ID, PROJECT_FILE_TYPE, DOWNLOAD_URL, PROTOCOL, PROTOCOL_NAME) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+        return update(query, projectFile.getFile_key(), projectFile.getRgdid(), projectFile.getProject_file_type(), projectFile.getDownload_url(), projectFile.getProtocol(), projectFile.getProtocol_name());
+    }
 
+    public int update(ProjectFile projectFile) throws Exception {
+        String query = "UPDATE PROJECT_FILES SET PROJECT_FILE_TYPE=?, DOWNLOAD_URL=?, PROTOCOL=?, PROTOCOL_NAME=? WHERE FILE_KEY=?";
+        return update(query, projectFile.getProject_file_type(), projectFile.getDownload_url(), projectFile.getProtocol(), projectFile.getProtocol_name(), projectFile.getFile_key());
+    }
+
+    public int delete(int fileKey) throws Exception {
+        String query = "DELETE FROM PROJECT_FILES WHERE FILE_KEY=?";
+        return update(query, fileKey);
+    }
+    
 }
