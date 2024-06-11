989ad5e5f4c013ae8c6fac9f8bbda03cfcbe2208
llamersmcw <llamers@mcw.edu>
2024-06-11 11:07:34 -0500

    new method for geneExpressionRecordValues

--- src/edu/mcw/rgd/dao/impl/GeneExpressionDAO.java
+++ src/edu/mcw/rgd/dao/impl/GeneExpressionDAO.java
@@ -268,6 +268,20 @@ public class GeneExpressionDAO extends PhenominerDAO {
         GeneExpressionRecordValueQuery q = new GeneExpressionRecordValueQuery(getDataSource(), query);
         return execute(q, termAcc,rgdId,unit,level);
     }
+    /**
+     * Returns a list of values for given gene expression experiment record
+     * @return list of GeneExpressionRecordValue objects; could be empty
+     * @throws Exception
+     */
+    public List<GeneExpressionRecordValue> getGeneExprRecordValuesForGeneByTermRgdIdUnit(int rgdId,String unit,String termAcc) throws Exception {
+        String query = "select ge.* FROM gene_expression_values ge join gene_expression_exp_record gr on ge.gene_expression_exp_record_id = gr.gene_expression_exp_record_id" +
+                " join sample s on s.sample_id = gr.sample_id join ont_terms t on t.term_acc = s.tissue_ont_id where  t.term_acc IN(SELECT child_term_acc FROM ont_dag START WITH parent_term_acc=?" +
+                " CONNECT BY PRIOR child_term_acc=parent_term_acc ) AND t.is_obsolete=0 and ge.expressed_object_rgd_id=? and ge.expression_unit =?" +
+                " order by ge.gene_expression_exp_record_id";
+
+        GeneExpressionRecordValueQuery q = new GeneExpressionRecordValueQuery(getDataSource(), query);
+        return execute(q, termAcc,rgdId,unit);
+    }
 
     /**
      * Returns count values for given gene
