5588e527e41ff37c70d8a5f9ecf8c1a7542617f4
Akhilanand K <kundurthiakhil@gmail.com>
2024-09-17 12:56:42 -0500

    Added last modified date to the reference object

--- src/edu/mcw/rgd/datamodel/Reference.java
+++ src/edu/mcw/rgd/datamodel/Reference.java
@@ -28,6 +28,7 @@ public class Reference implements Identifiable, Speciated {
     private String urlWebReference;
     private int speciesTypeKey;
     private String doi;
+    private String lastModifiedDate;
 
     public String getDoi() {
         return doi;
@@ -179,5 +180,13 @@ public class Reference implements Identifiable, Speciated {
 
     public void setSpeciesTypeKey(int speciesTypeKey) {
         this.speciesTypeKey = speciesTypeKey;
+    }
+
+    public String getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(String lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
     }
 }
