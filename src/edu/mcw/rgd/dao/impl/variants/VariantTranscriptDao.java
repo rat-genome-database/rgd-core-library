cc402d8751749e585f561963f51a0bca99a1780f
Thota <jthota@mcw.edu>
2024-12-26 12:34:33 -0600

    fixed genesymbol vv details

--- src/edu/mcw/rgd/dao/impl/variants/VariantTranscriptDao.java
+++ src/edu/mcw/rgd/dao/impl/variants/VariantTranscriptDao.java
@@ -13,6 +13,7 @@ public class VariantTranscriptDao extends AbstractDAO {
     public List<edu.mcw.rgd.datamodel.variants.VariantTranscript> getVariantTranscripts(long rgdId, int mapKey) throws Exception {
         String sql=" select t.*, p.prediction from variant_transcript t left outer join " +
                 "                polyphen p on (t.variant_rgd_id=p.variant_rgd_id and t.transcript_rgd_id=p.transcript_rgd_id)\n" +
+                " inner join transcripts tr on tr.transcript_rgd_id=t.transcript_rgd_id" +
                 "                where t.variant_rgd_id=? " +
                 "                and t.map_key=?";
         VariantTranscriptQuery q=new VariantTranscriptQuery(DataSourceFactory.getInstance().getCarpeNovoDataSource(), sql);
