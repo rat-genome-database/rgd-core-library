707f914db8e64db2605c463afd93a6e765bfe5b6
Thota <jthota@mcw.edu>
2025-02-09 19:26:24 -0600

    updated

--- src/edu/mcw/rgd/datamodel/solr/PubmedSolrDoc.java
+++ src/edu/mcw/rgd/datamodel/solr/PubmedSolrDoc.java
@@ -1,10 +1,9 @@
 package edu.mcw.rgd.datamodel.solr;
 
+import org.apache.solr.common.SolrInputDocument;
+
 class PubmedSolrDoc extends SolrInputDocument {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
 
     public void addField(String fieldName, Object value) {
